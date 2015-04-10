b115d186a08ee2d994c98944f1eed9ba9fe2e086
MTTHWBSH <bushmat4@gmail.com>
2015-04-09 22:12:38 -0500

    keep on keepin on

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,8 +1,9 @@
 (function() {
-  var heroSize, showContent;
+  var blog, heroSize, showContent;
 
   $(document).ready(function() {
     heroSize();
+    blog.init;
   });
 
   $(window).load(function() {
@@ -38,6 +39,53 @@
 
   showContent = function() {
     $(".page-content").addClass("load");
+  };
+
+  blog = {
+    loadMorePosts: function() {
+      var $blogContainer, _this, nextPage, totalPages;
+      _this = this;
+      $blogContainer = $('.post-list');
+      nextPage = parseInt($blogContainer.attr('data-page')) + 1;
+      totalPages = parseInt($blogContainer.attr('data-totalPages'));
+      $(this).addClass('loading');
+      $.get('/blog/page' + nextPage + '/', function(data) {
+        var $articles, htmlData;
+        htmlData = $.parseHTML(data);
+        $articles = $(htmlData).find('article');
+        $blogContainer.attr('data-page', nextPage).append($articles);
+        if ($blogContainer.attr('data-totalPages') === nextPage) {
+          $('.load-more-posts').remove();
+        }
+        $(_this).removeClass('loading');
+      });
+    },
+    bindUiEvents: function() {
+      $('pre').click(function() {
+        $(this).toggleClass('expanded');
+      });
+      $('.blog__back-to-site').click(function() {
+        var _this;
+        _this = this;
+        $('body').fadeOut('slow', function() {
+          window.location.href = $(_this).attr('href');
+        });
+        return false;
+      });
+      $('.load-more-posts').click(blog.loadMorePosts);
+    },
+    bindGAEvents: function() {
+      $('section.comments form').submit(function() {
+        ga('send', 'event', 'Comments', 'New comment');
+      });
+    },
+    init: function() {
+      if (document.referrer === 'http://oakdaleboutique.com/' || document.referrer === 'http://oakdaleboutique.com/#' || document.referrer === 'https://oakdaleboutique.com/' || document.referrer === 'https://oakdaleboutique.com/#') {
+        $('body').hide().fadeIn('slow');
+      }
+      blog.bindUiEvents();
+      blog.bindGAEvents();
+    }
   };
 
 }).call(this);
--- js/main.coffee
+++ js/main.coffee
@@ -4,6 +4,7 @@
 
 $(document).ready ->
 	heroSize()
+	blog.init
 	return
 
 $(window).load ->
@@ -33,3 +34,43 @@ heroSize = ->
 showContent = ->
 	$(".page-content").addClass "load"
 	return
+
+blog = 
+  loadMorePosts: ->
+    _this = this
+    $blogContainer = $('.post-list')
+    nextPage = parseInt($blogContainer.attr('data-page')) + 1
+    totalPages = parseInt($blogContainer.attr('data-totalPages'))
+    $(this).addClass 'loading'
+    $.get '/blog/page' + nextPage + '/', (data) ->
+      htmlData = $.parseHTML(data)
+      $articles = $(htmlData).find('article')
+      $blogContainer.attr('data-page', nextPage).append $articles
+      if $blogContainer.attr('data-totalPages') == nextPage
+        $('.load-more-posts').remove()
+      $(_this).removeClass 'loading'
+      return
+    return
+  bindUiEvents: ->
+    $('pre').click ->
+      $(this).toggleClass 'expanded'
+      return
+    $('.blog__back-to-site').click ->
+      _this = this
+      $('body').fadeOut 'slow', ->
+        window.location.href = $(_this).attr('href')
+        return
+      false
+    $('.load-more-posts').click blog.loadMorePosts
+    return
+  bindGAEvents: ->
+    $('section.comments form').submit ->
+      ga 'send', 'event', 'Comments', 'New comment'
+      return
+    return
+  init: ->
+    if document.referrer == 'http://oakdaleboutique.com/' or document.referrer == 'http://oakdaleboutique.com/#' or document.referrer == 'https://oakdaleboutique.com/' or document.referrer == 'https://oakdaleboutique.com/#'
+      $('body').hide().fadeIn 'slow'
+    blog.bindUiEvents()
+    blog.bindGAEvents()
+    return
